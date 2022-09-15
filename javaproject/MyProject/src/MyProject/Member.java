885544cc2e630786a0beaa4f39b3a9eefddd9624
skjung1979 <skjung1979.dvlp@gmail.com>
2022-09-16 02:26:19 +0900

    chapter04 문제풀이 - jsk
    chapter04 문제풀이 - jsk

--- javaproject/MyProject/src/MyProject/Member.java
+++ javaproject/MyProject/src/MyProject/Member.java
@@ -3,9 +3,11 @@ package MyProject;
 import java.util.Calendar;
 import java.util.Scanner;
 
-public class Member {
+public class Member
+{
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
 		aaaMyName("정성균");
 		
@@ -21,7 +23,7 @@ public class Member {
 		System.out.println(hasBook);
 		
 		
-		/* chapter04 문제 풀이 - 정성균
+		/* chapter04-01 문제 풀이 - 정성균
 		 * 
 		 * ① 독감예방 접종이 가능한지 여부를 확인하는 메소드를 정의합니다.
 		 * - 매개변수로 태어난 해(년도)를 전달받습니다.
@@ -49,7 +51,7 @@ public class Member {
 		System.out.println("============================");
 		
 	
-		
+		checkHealth(availAge, birthYear);
 		
 		
 		
@@ -62,12 +64,15 @@ public class Member {
 		
 	}
 	
-	public static void checkDok(int availAge) {
+	public static void checkDok(int availAge)
+	{
 	 
 	
-	 		if(availAge >= 65 || availAge < 15) {
+	 		if(availAge >= 65 || availAge < 15)
+	 		{
 	 		    System.out.println("무료예방접종이 가능합니다.");
-	 		}else {
+	 		}else
+	 		{
 	 		    System.out.println("무료접종 대상이 아닙니다.");
 	 		}
 	 		
@@ -75,9 +80,11 @@ public class Member {
 	    
 	}
 	
-	public static void checkHealth(int availAge, int birthYear) {
+	public static void checkHealth(int availAge, int birthYear)
+	{
 	    
-		/*
+		/*chapter04-02 문제풀이 - 정성균
+		 * 
 		 * ② 건강검진 대상 여부를 판별하고 어떤 검진이 가능한지 확인하는 메소드를 정의합니다.
 		 * - 매개변수로 태어난 해(년도)를 전달받습니다.
 		 * - 대한민국 성인( 20세 )의 경우 무료로 2년마다 건강검진을 받을 수 있습니다.
@@ -86,23 +93,36 @@ public class Member {
 		 */
 	    int year = Calendar.getInstance().get(Calendar.YEAR);
 	    
-		if(availAge >= 20 && year%2 == birthYear%2) {
-		    // 건강검진 대상이된다. 하지만 2년마다
-		    if(availAge >= 40) {
-			System.out.println("암 검진도 가능");
-		    }
+	    // 20세 이상이면 참 -> 무료 건강 검진
+		if(availAge >= 20)
+		{
+		    // 40세 이상이면 무료검진+무료암검진
+		    if(availAge >= 40) // 40세 이상이면 무료 검진 + 무료 암검진
+		    {
+		    	System.out.println("무료 건강 검진 가능하며, 암 검진도 무료 검진 가능합니다.");
+				if(birthYear%2==0 && year%2==0)
+				{
+			    System.out.println("올해 무료 검진 받으세요.");
+				}else
+				{
+			    System.out.println("내년에 무료 검진 받으세요.");
+				}
 		    
-		    
-		    
-		    
-		    
-		}else {
-		    
+			}else // 20세 이상이고 40세 이하라면
+			{	 
+				//2년에 한번 기본 검사만 가능하다.
+				if(birthYear%2==0 && year%2==0)
+				{
+			    System.out.println("올해 무료 검진 받으세요.");
+				}else
+				{
+			    System.out.println("내년에 무료 검진 받으세요.");
+				}
+			}
+		}else
+		{
+			// 20세 미만인 사람에 대한 안내 문구
+			System.out.println("무료 건강 검진 대상이 아닙니다.");
 		}
-	    
-	}
-
-	
-	
-	
+	}	
 }
