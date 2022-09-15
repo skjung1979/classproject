4bf72a5d01c4a71b3d19d1339a98b3a22beaadaf
skjung1979 <skjung1979.dvlp@gmail.com>
2022-09-16 02:31:16 +0900

    현재 년도에 대한 주석 추가
    현재 년도에 대한 주석 추가

--- javaproject/MyProject/src/MyProject/Member.java
+++ javaproject/MyProject/src/MyProject/Member.java
@@ -31,7 +31,7 @@ public class Member
 		 * - 위에서 정의한 조건의 범위가 아닌 나머지의 경우 “무료접종 대상이 아닙니다.”라고 출력하도록 합니다.
 		 */
 		
-		
+		// 현재 날짜에서 년도만 int에 넣기
 		int year = Calendar.getInstance().get(Calendar.YEAR);
 		
 		// 매개변수로 태어난 해(년도)를 전달받습니다.
@@ -91,6 +91,8 @@ public class Member
 		 * - 짝수 해에 태어난 사람은 올해가 짝수년이라면 검사 대상이 됩니다.
 		 * - 40 이상의 경우는 암 검사도 무료로 검사를 할 수 있습니다.
 		 */
+		
+		// 현재 날짜에서 년도만 int에 넣기
 	    int year = Calendar.getInstance().get(Calendar.YEAR);
 	    
 	    // 20세 이상이면 참 -> 무료 건강 검진
