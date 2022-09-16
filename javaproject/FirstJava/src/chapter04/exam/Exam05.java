a18cd80493993fab865eeaecbc96d9c21069e99c
skjung1979 <skjung1979.dvlp@gmail.com>
2022-09-17 07:47:20 +0900

    카페 반복문 연습문제 04-06 복습
    카페 반복문 연습문제 04-06 복습

--- javaproject/FirstJava/src/chapter04/exam/Exam05.java
+++ javaproject/FirstJava/src/chapter04/exam/Exam05.java
@@ -65,7 +65,68 @@ public class Exam05 {
 //		}
 		
 		
+		// ========= 카페 '반복문 연습 문제' ========= 정성균 복습 문제풀이
+
+//			문제 5.
+//			n=1 일 때  “현재 인원은 1명 입니다.”
+//			n=2 일 때  “현재 인원은 2명 입니다.”
+//			n=3 일 때  “현재 인원은 3명 입니다.”
+//			n>3 일 때  “현재 많은 사람들이 있습니다.”
+		
+		System.out.println("--------복습-----------");
+		
+		int n1 = 7;
+		
+		switch(n1)
+		{
+		case 1:
+			System.out.println("현재 인원은 1명 입니다.");
+			break;
+		case 2:
+			System.out.println("현재 인원은 2명 입니다.");
+			break;
+		case 3:
+			System.out.println("현재 인원은 3명 입니다.");
+			break;
+		default:
+			if(n==0)
+			{
+				System.out.println("현재 인원은 없습니다.");
+			}else
+			{
+				System.out.println("현재 인원은 "+ n1 +"명 입니다.");
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- javaproject/FirstJava/src/chapter04/exam/Exam06.java
+++ javaproject/FirstJava/src/chapter04/exam/Exam06.java
@@ -41,5 +41,61 @@ public class Exam06 {
 		System.out.println(sum);
 		
 		
+//  ========= 카페 '반복문 연습 문제' ========= 정성균 문제풀이
+		System.out.println("----------복습-----------");
+		
+//			문제 6.
+//			1 부터 99까지의 합을 구하는 프로그램 작성
+//			while문, for문, do while 문을 각각 사용
+
+		// while문 이용
+//		while(조건식)
+//		{
+//			참이면 실행되는 코드;
+//			증감식;
+//		}
+		
+		int n1 = 1; // 증가 변수. 초기값은 합이므로 0
+		int sum1 = 0;
+		
+		while(n1<100)
+		{
+			sum1 += n1; // sum1 = sum1 + n1
+			n1++;
+		} System.out.println("while문을 이용한 1~99까지의 합: "+ sum1);
+		
+		// do while문 이용
+//		do
+//		{
+//			참이면 실행되는 모드(초기 한번은 조건에 상관없이 무조건 실행된다.);
+//			증감식;
+//		}while(조건식);
+		
+		int n2 = 1;
+		int sum2 = 0;
+		
+		do
+		{
+			sum2 += n2;
+			n2++;
+		}while(n2<100);
+		System.out.println("do while문을 이용한 1~99까지의 합: "+ sum2);
+		
+		//  for문 이용
+//		for(초기값; 조건; 증감식)
+//		{
+//			참일 때 실행도는 코드;
+//		}
+		
+		int sum3 = 0;
+		for(int n3=1; n3<100; n3++)
+		{
+			sum3 += n3;
+		} System.out.println("for문을 이용한 1~99까지의 합: "+ sum3);
+		
+		
+		
+		
+		
 	}
 }
