3c17ce26a5c6fb463906849acba8427150d52ce3
skjung1979 <skjung1979.dvlp@gmail.com>
2022-09-17 10:51:00 +0900

    형변환&반복문 연습
    형변환&반복문 연습

---
+++ javaproject/MyProject/src/Q200/CastingRangeMain.java
@@ -0,0 +1,20 @@
+package Q200;
+
+public class CastingRangeMain {
+
+	public static void main(String[] args) {
+
+		
+		for(int i=65; i<=165; i++)
+		{
+			System.out.print((char)i+" ");
+			
+			if(i%10==0)
+			{
+				System.out.println();
+			}
+		}
+
+	}
+
+}
