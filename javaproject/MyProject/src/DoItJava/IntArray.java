c51c6209ebd682c8ce80b52380964685fbf49bf6
skjung1979 <skjung1979.dvlp@gmail.com>
2022-09-19 01:45:40 +0900

    배열 연습 - 가장 큰 키는
    배열 연습 - 가장 큰 키는

--- javaproject/MyProject/src/DoItJava/IntArray.java
+++ javaproject/MyProject/src/DoItJava/IntArray.java
@@ -1,7 +1,14 @@
 package DoItJava;
 
+import java.util.Scanner;
+
 public class IntArray {
 
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// 구성 요소의 자료형이 int형인 배열(구성 요소수는 5 / new에 의해 본체를 생성)
 		
@@ -14,7 +21,49 @@ public class IntArray {
 			
 			System.out.println("a["+i+"] = "+a[i]);
 		}
+		
+		// 배열에서 최대값 구하기
+		
+		int max = a[0];
 
+		
+		
+		for(int i=1;i<a.length;i++)
+		{
+			if(a[i]>max) max = a[i];
+		}
+		System.out.println("최대값은 "+max);
+		
+		// 실습. 키의 최대값을 구합니다
+		// 사람수와 키값을 입력 받는다.
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("키의 최대값을 구합니다. 사람수와 키를 입력해주세요.");
+		System.out.print("사람수는? >>");
+		int nPeople = scan.nextInt();
+		System.out.println();
+		System.out.println("키는? >>");
+		int[] height = new int[nPeople];
+		
+		for(int i=0; i<nPeople;i++)
+		{
+			System.out.print(i+"번째 사람의키: ");
+			height[i] = scan.nextInt();
+		}
+		
+		System.out.println("이중에 가장 큰 키는 "+ maxOf(height) + "입니다.");
+			
 	}
+	static int maxOf(int[] a)
+	{
+		int max = a[0];
+		for(int i=1; i<a.length; i++)
+		{
+			if(a[i]>max) max = a[i];
+		}
+		return max;
+	}
+
 
 }
