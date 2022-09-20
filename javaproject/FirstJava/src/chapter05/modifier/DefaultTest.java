e06bd8f49c0e70fdef91cf6a95556d708ead0153
skjung1979 <skjung1979.dvlp@gmail.com>
2022-09-20 15:27:30 +0900

    dafault
    dafault

---
+++ javaproject/FirstJava/src/chapter05/modifier/DefaultTest.java
@@ -0,0 +1,8 @@
+package chapter05.modifier;
+
+public class DefaultTest {
+	
+	String name;
+	int age;
+
+}
---
+++ javaproject/FirstJava/src/chapter05/modifier/main/DefaultTestMain.java
@@ -0,0 +1,14 @@
+package chapter05.modifier.main;
+
+import chapter05.modifier.DefaultTest;
+
+public class DefaultTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// DefaultTestMain dt = new DefaultTest();
+		// 안된다? 다른 패키지라서?
+	}
+
+}
