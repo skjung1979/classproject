062642e52c2956abae5cc03175a8079889f943a0
skjung1979 <skjung1979.dvlp@gmail.com>
2022-09-21 12:43:46 +0900

    배열 예제 4 - {}로 생성
    배열 예제 4 - {}로 생성

--- javaproject/FirstJava/src/chapter06/ArrayTest4.java
+++ javaproject/FirstJava/src/chapter06/ArrayTest4.java
@@ -32,6 +32,28 @@ public class ArrayTest4 {
 				System.out.print("arr["+i+"]"+"["+j+"] = "+ arr[i][j]+"\t");
 			}
 			System.out.println(); // i가 바뀌면 개행
-		}	
+		}
+		
+		// 2차원 배열의 초기화
+		int[][] scores =
+			{	// 각각의 1차원 배열은 3개의 요소를 갖는다.
+					{11,12,13},
+					{21,22,23},
+					{31,32,33},
+					{41,42,43},
+			};
+		
+		// i = 0 1 2 3
+		for(int i=0; i<scores.length; i++)
+		{
+			for(int j=0; j<scores[i].length; j++)
+			{
+				System.out.print(scores[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		
+		
+		
 	}
 }
