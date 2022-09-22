9a986007d421a57e168cca1e6988a156813479f4
skjung1979 <skjung1979.dvlp@gmail.com>
2022-09-22 09:27:28 +0900

    배열 역순 연습
    배열 역순 연습

---
+++ javaproject/MyProject/src/DoItJava/ReverseArray.java
@@ -0,0 +1,45 @@
+package DoItJava;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseArray
+{
+	
+	static void swap(int[] a, int idx1, int idx2)
+	{
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	
+	static void reverse(int[] a)
+	{
+		for(int i=0; i<a.length/2; i++)
+		{
+			swap(a, i, a.length-i-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("요소수는? ");
+		int num = scan.nextInt();
+		
+		int[] x = new int[num];
+		
+		for(int i=0; i<num;i++)
+		{
+			System.out.print("x[" + i + "]: ");
+			x[i] = scan.nextInt();
+		}
+		
+		reverse(x);
+		
+		System.out.println("요소를 역순으로 정렬했습니다.");
+		System.out.println("x = "+ Arrays.toString(x));
+	
+	}
+	
+}
