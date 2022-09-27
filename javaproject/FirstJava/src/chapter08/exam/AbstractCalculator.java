a72cff4d755d7d269ef762b6f65eb8f04301900b
skjung1979 <skjung1979.dvlp@gmail.com>
2022-09-28 06:50:41 +0900

    연습문제08
    연습문제08

---
+++ javaproject/FirstJava/src/chapter08/exam/AbstractCalculator.java
@@ -0,0 +1,9 @@
+package chapter08.exam;
+
+// 1. Calulator 인터페이스를 상속하는 추상 클래스를 정의해봅시다. 
+public abstract class AbstractCalculator implements Calculator {
+
+	private double pi = 3.14;	// 이런식으로 변수 추가 선언 가능
+	
+	
+}
---
+++ javaproject/FirstJava/src/chapter08/exam/CalculatorImpl.java
@@ -0,0 +1,61 @@
+package chapter08.exam;
+
+// 2. Calulator 인터페이스를 상속하는 인스턴스를 생성할 수 있는 클래스를 정의해봅시다. 
+public class CalculatorImpl implements Calculator {
+
+	@Override
+	public long add(long n1, long n2) {
+		// TODO Auto-generated method stub
+		return n1+n2;
+	}
+
+	@Override
+	public long substract(long n1, long n2) {
+		// TODO Auto-generated method stub
+		return n1-n2;
+	}
+
+	@Override
+	public long multiply(long n1, long n2) {
+		// TODO Auto-generated method stub
+		return n1*n2;
+	}
+
+	@Override
+	public double divide(double n1, double n2) {
+		// TODO Auto-generated method stub
+		return n1/n2;
+	}
+
+	public static void main(String[] args) {
+		
+// 3. 다형성의 특징으로 상위 타입인 인터페이스 타입의 참조변수에 인터페이스를 구현한 클래스 타입의 인스턴스를 참조하는 코드를 작성해 봅시다.
+		CalculatorImpl cal1 = new CalculatorImpl();
+		Calculator cal2 = new CalculatorImpl();
+
+		Calculator  cal3 = new Calculator() {	// 익명클래스
+			// 아래 오러라이딩 코드는 한번만 사용되는 것이다.
+			@Override
+			public long substract(long n1, long n2) {
+				return n1-n2;
+			}
+			
+			@Override
+			public long multiply(long n1, long n2) {
+				return n1*n2;
+			}
+			
+			@Override
+			public double divide(double n1, double n2) {
+				return n1/n2;
+			}
+			
+			@Override
+			public long add(long n1, long n2) {
+				return n1+n2;
+			}
+		};
+		
+	}
+	
+}
