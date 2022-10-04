4d7412a78e16aac9b8f01432ae292c95d8a7f4ed
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-04 13:03:26 +0900

    ...
    ...

--- javaproject/FirstJava/src/chapter11/MyClass.java
+++ javaproject/FirstJava/src/chapter11/MyClass.java
@@ -4,7 +4,7 @@ public class MyClass<T> {
 
 	T val;
 	
-	T getVal(){
+	T getVal(){ // get으로 메소드 이름을 정한다면 return이 기본적으로 따라옴을 생각해야함.
 		return val;
 	}
 	
@@ -14,7 +14,7 @@ public class MyClass<T> {
 	
 	public static void main(String[] args) {
 		
-		// 제네릭 클래스는 인스턴스 생성시에 타입을 결정한다.
+		// 제네릭 클래스는 인스턴스 생성시에 타입을 결정한다. 클래스블럭에서가 아니고,, 생성시!!!
 		MyClass<String> myClass = new MyClass<String>();
 		String str = myClass.getVal();
 		
