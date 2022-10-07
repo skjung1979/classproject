fdb50c622cc6a606d7d9ce7167075155cd2960e0
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-07 16:20:51 +0900

    ㅂㅕㄹ
    ㅂㅕㄹ

--- javaproject/MyProject/src/Q200/StarPoint50t3.java
+++ javaproject/MyProject/src/Q200/StarPoint50t3.java
@@ -5,16 +5,21 @@ public class StarPoint50t3 {
 	public static void main(String[] args) {
 
 		//printStarDec(5); // 행의 개수이다.
-		printStarInc(5);
+		printStarDec1(5);
+		printStarDec2(5);
+		printStarDec3(5);
+		
+		printStarInc1(5);
+		printStarInc2(5);
 		
 	}
 	
-	// 별 감소 메소드
-	public static void printStarDec(int n) {
+	// 별 감소 메소드 1
+	public static void printStarDec1(int n) {
 		
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<i; j++) {
-				System.out.print("-");
+				System.out.print(" ");
 			}
 			for(int k=1; k<2*(n-i); k++) {
 				System.out.print("*");
@@ -24,18 +29,55 @@ public class StarPoint50t3 {
 		
 	}
 	
-	// 별 증가 메소드
-	public static void printStarInc(int n) {
+	// 별 감소 메소드 2
+	public static void printStarDec2(int n) {
 		
 		for(int i=0; i<n; i++) {
-			for(int j=n-1; j>=0; j--) {
-				System.out.print("-");
+			for(int j=n-i; j>0; j--) {
+				System.out.print("*");
 			}
 			System.out.println();
 		}
 		
 	}
 	
+	// 별 감소 메소드 3
+	public static void printStarDec3(int n) {
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=n-i; k>0; k--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	// 별 증가 메소드 1
+	public static void printStarInc1(int n) {
+		
+		for(int i=0; i<n; i++) {
+			for(int j=n-i-1; j>0; j--) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<=i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}	
+	}
+	
+	// 별 증가 메소드 2
+	public static void printStarInc2(int n) {
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 }
 
 
