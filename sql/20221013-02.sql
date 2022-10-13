79fd349c28cbf304e5f857c980fad96a4030b613
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-13 10:14:09 +0900

    Update 20221013-02.sql

--- sql/20221013-02.sql
+++ sql/20221013-02.sql
@@ -10,10 +10,13 @@ select bookname from book where price>20000;
 select name, address from customer;
 
 --(4) 2014년7월4일~7월7일사이에주문받은도서의주문번호
-select * from orders where orderdate between 14/07/04 and 14/07/07;
+select orderid from orders where orderdate between '14/07/04' and '14/07/07';
 
 --(5) 2014년7월4일~7월7일사이에주문받은도서를제외한도서의주문번호
+select orderid from orders where orderdate not between '14/07/04' and '14/07/07';
 
 --(6) 성이‘김’씨인고객의이름과주소
+select name, address from customer where name like '김%';
 
 --(7) 성이‘김’씨이고이름이‘아’로끝나는고객의이름과주소
+select name, address from customer where name like '김%아';
