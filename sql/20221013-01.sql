1f26fec83a8b93b1381cdd5302a68df55e754664
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-13 11:21:12 +0900

    Update 20221013-01.sql

--- sql/20221013-01.sql
+++ sql/20221013-01.sql
@@ -1,18 +1,20 @@
 -- EMP 테이블과 DEPT 테이블을 이용해서 풀이해 주세요.
 -- 1. 덧셈연산자를 이용하여 모든 사원에 대해서 $300의 급여 인상을 계산한 후 사원의 이름, 급여, 인상된 급여를 출력하시오.
-select ename, sal, sal+300 from emp;
+select ename, sal, sal+300 as add_Sal from emp order by 3;
+-- order by에 컬럼명 대신 숫자로 컬럼을 지정할 수 있다. 하지만 별칭을 주어 order by 하는 것을 권장한다.
 
 -- 2. 사원의 이름, 급여, 연간 총 수입을 총 수입이 많은 것부터 작은 순으로 출력하시오, 연간 총수입은 월급에 12를 곱한 후 $100의 상여금을 더해서 계산하시오.
-select ename, sal, sal*12+100 yearSal from emp order by yearSal desc;
+select ename, sal, sal*12+100 as year_sal from emp order by year_sal desc;
 
 -- 3. 급여가 2000을 넘는 사원의 이름과 급여를 표현, 급여가 많은 것부터 작은 순으로 출력하시오.
-select ename, sal from emp where sal>2000 order by sal asc;
+select ename, sal from emp where sal>2000 order by sal desc;
 
 -- 4. 사원번호가 7788인 사원의 이름과 부서번호를 출력하시오.
 select ename, deptno from emp where empno=7788;
 
 -- 5. 급여가 2000에서 3000 사이에 포함되지 않는 사원의 이름과 급여를 출력하시오.
 select ename, sal from emp where sal not between 2000 and 3000;
+select ename, sal from emp where not sal  between 2000 and 3000;
 
 -- 6. 1981년 2월 20일 부터 1981년 5월 1일 사이에 입사한 사원의 이름, 담당업무, 입사일을 출력하시오.
 select ename, job, hiredate from emp where hiredate between '81/02/20' and '81/05/01';
@@ -27,7 +29,7 @@ select ename, sal, deptno from emp where sal between 2000 and 3000 and deptno in
 select ename, hiredate from emp where hiredate like '81%' ;
 
 -- 10. 관리자가 없는 사원의 이름과 담당 업무를 출력하시오.
-select ename, job from emp where meg is null;
+select ename, job from emp where mgr is null;
 
 -- 11. 커미션을 받을 수 있는 자격이 되는 사원의 이름, 급여, 커미션을 출력하되 급여 및 커미션을 기준으로 내림차순 정렬하여 표시하시오.
 select ename, sal, comm from emp where comm is not null order by sal desc, comm desc;
