be1af30852a949a142dac24223bef334d1e28fe2
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-18 06:37:04 +0900

    Update 20221017_exam_Join&SubQuery.sql

--- sql/20221017_exam_Join&SubQuery.sql
+++ sql/20221017_exam_Join&SubQuery.sql
@@ -8,91 +8,52 @@ from book a, orders b, customer c
 where a.bookid=b.bookid and b.custid=c.custid and c.name='박지성';
 
 -- (6) 박지성이구매한도서의이름, 가격, 정가와판매가격의차이
-
-
-​
+select b.bookname, b.price, abs(o.saleprice-b.price) as pricegap 
+from orders o, book b, customer c 
+where o.bookid=b.bookid and o.custid=c.custid and c.name='박지성';
 
 -- (7) 박지성이구매하지않은도서의이름
-
-
-​
-
-​
+select bookname 
+from book 
+where bookid not in (
+select bookid from orders o left outer join customer c on o.custid=c.custid
+where c.name='박지성'
+);
 
 -- 2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
-
 -- (8) 주문하지않은고객의이름(부속질의사용)
-
-
-​
-
-​
+select name from customer where custid not in (select custid from orders);
 
 -- (9) 주문금액의총액과주문의평균금액
-
-
-​
-
-​
-
-​
+select sum(saleprice) total, avg(saleprice) avg_saleprice from orders;
 
 -- (10) 고객의이름과고객별구매액
-
-
-​
-
-​
-
-​
-
-​
+select c.name, sum(o.saleprice) from customer c right join orders o on c.custid=o.custid group by c.name;
 
 -- (11) 고객의이름과고객이구매한도서목록
-
-
-​
-
-​
-
-​
-
-​
+select c.name, b.bookname from customer c, orders o, book b where c.custid=o.custid and o.bookid=b.bookid order by c.name, b.bookname;
 
 -- (12) 도서의가격(Book 테이블)과판매가격(Orders 테이블)의차이가가장많은주문
-
-
-​
-
-​
+select * from orders o, book b 
+where o.bookid=b.bookid 
+and abs(b.price-o.saleprice)=(select max(abs(b.price-o.saleprice)) from book b, orders o where b.bookid=o.bookid);
 
 -- (13) 도서의판매액평균보다자신의구매액평균이더높은고객의이름
-
-
-​
-
-​
-
-​
+select c.name from customer c, (
+select custid, avg(saleprice) as avg_saleprice from orders group by custid
+) d 
+where c.custid=d.custid 
+and d.avg_saleprice>(select avg(saleprice) from orders);
 
 -- 3. 마당서점에서 다음의 심화된 질문에 대해 SQL 문을 작성하시오.
-
 -- (1) 박지성이 구매한 도서의 출판사와 같은 출판사에서 도서를 구매한 고객의 이름
-
-
-​
-
-​
+select distinct c.name from book b, orders o, customer c 
+where b.bookid=o.bookid and o.custid=c.custid
+ and b.publisher in (select b.publisher from book b, orders o, customer c where b.bookid=o.bookid and o.custid=c.custid and c.name='박지성')
+ and c.name <> '박지성';
 
 -- (2) 두 개 이상의 서로 다른 출판사에서 도서를 구매한 고객의 이름
-
-
-
-
-
-
-
-
-
-
+select c.name from customer c, (
+select o.custid, count(distinct b.publisher) as cnt from orders o left join book b on o.bookid=b.bookid group by o.custid having (count(distinct b.publisher))>=2 order by o.custid
+) d where c.custid=d.custid;
 
