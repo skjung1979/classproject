c622bdee5c5f544a81fc81ae51194ccfc4099d1e
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-24 10:47:00 +0900

    ...

--- sql/MiniProject_OneTeam_hall.sql
+++ sql/MiniProject_OneTeam_hall.sql
@@ -1,30 +1,30 @@
-DROP INDEX SCOTT.PK_hall;
+DROP INDEX ONE.PK_hall;
 
 /* hall */
-DROP TABLE SCOTT.hall 
+DROP TABLE ONE.hall 
 	CASCADE CONSTRAINTS;
 
 /* hall */
-CREATE TABLE SCOTT.hall (
+CREATE TABLE hall (
 	hallNo INTEGER NOT NULL, /* hallNo */
 	hallResvYN VARCHAR2(50), /* hallResvYN */
 	hallTime INTEGER /* hallTime */
 );
 
-COMMENT ON TABLE SCOTT.hall IS 'hall';
+COMMENT ON TABLE ONE.hall IS 'hall';
 
-COMMENT ON COLUMN SCOTT.hall.hallNo IS 'hallNo';
+COMMENT ON COLUMN ONE.hall.hallNo IS 'hallNo';
 
-COMMENT ON COLUMN SCOTT.hall.hallResvYN IS 'hallResvYN';
+COMMENT ON COLUMN ONE.hall.hallResvYN IS 'hallResvYN';
 
-COMMENT ON COLUMN SCOTT.hall.hallTime IS 'hallTime';
+COMMENT ON COLUMN ONE.hall.hallTime IS 'hallTime';
 
-CREATE UNIQUE INDEX SCOTT.PK_hall
-	ON SCOTT.hall (
+CREATE UNIQUE INDEX ONE.PK_hall
+	ON ONE.hall (
 		hallNo ASC
 	);
 
-ALTER TABLE SCOTT.hall
+ALTER TABLE ONE.hall
 	ADD
 		CONSTRAINT PK_hall
 		PRIMARY KEY (
--- sql/MiniProject_OneTeam_reservation.sql
+++ sql/MiniProject_OneTeam_reservation.sql
@@ -1,11 +1,11 @@
-DROP INDEX SCOTT.PK_reservation;
+DROP INDEX ONE.PK_reservation;
 
 /* reservation */
-DROP TABLE SCOTT.reservation 
+DROP TABLE ONE.reservation 
 	CASCADE CONSTRAINTS;
 
 /* reservation */
-CREATE TABLE SCOTT.reservation (
+CREATE TABLE ONE.reservation (
 	reservNo INTEGER NOT NULL, /* reservNo */
 	memberNo INTEGER, /* memberNo */
 	hallNo INTEGER, /* hallNo */
@@ -13,46 +13,46 @@ CREATE TABLE SCOTT.reservation (
 	reservTime DATE /* reservTime */
 );
 
-COMMENT ON TABLE SCOTT.reservation IS 'reservation';
+COMMENT ON TABLE ONE.reservation IS 'reservation';
 
-COMMENT ON COLUMN SCOTT.reservation.reservNo IS 'reservNo';
+COMMENT ON COLUMN ONE.reservation.reservNo IS 'reservNo';
 
-COMMENT ON COLUMN SCOTT.reservation.memberNo IS 'memberNo';
+COMMENT ON COLUMN ONE.reservation.memberNo IS 'memberNo';
 
-COMMENT ON COLUMN SCOTT.reservation.hallNo IS 'hallNo';
+COMMENT ON COLUMN ONE.reservation.hallNo IS 'hallNo';
 
-COMMENT ON COLUMN SCOTT.reservation.reservDate IS 'reservDate';
+COMMENT ON COLUMN ONE.reservation.reservDate IS 'reservDate';
 
-COMMENT ON COLUMN SCOTT.reservation.reservTime IS 'reservTime';
+COMMENT ON COLUMN ONE.reservation.reservTime IS 'reservTime';
 
-CREATE UNIQUE INDEX SCOTT.PK_reservation
-	ON SCOTT.reservation (
+CREATE UNIQUE INDEX ONE.PK_reservation
+	ON ONE.reservation (
 		reservNo ASC
 	);
 
-ALTER TABLE SCOTT.reservation
+ALTER TABLE ONE.reservation
 	ADD
 		CONSTRAINT PK_reservation
 		PRIMARY KEY (
 			reservNo
 		);
 
-ALTER TABLE SCOTT.reservation
+ALTER TABLE ONE.reservation
 	ADD
 		CONSTRAINT FK_member_TO_reservation
 		FOREIGN KEY (
 			memberNo
 		)
-		REFERENCES SCOTT.member (
+		REFERENCES ONE.member (
 			memberNo
 		);
 
-ALTER TABLE SCOTT.reservation
+ALTER TABLE ONE.reservation
 	ADD
 		CONSTRAINT FK_hall_TO_reservation
 		FOREIGN KEY (
 			hallNo
 		)
-		REFERENCES SCOTT.hall (
+		REFERENCES ONE.hall (
 			hallNo
 		);
