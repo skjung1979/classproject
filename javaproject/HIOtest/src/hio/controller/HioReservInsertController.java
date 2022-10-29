f09b07dada7103ec26f539479d0d694f2651cd0b
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-29 21:36:24 +0900

    Update HioReservInsertController.java

--- javaproject/HIOtest/src/hio/controller/HioReservInsertController.java
+++ javaproject/HIOtest/src/hio/controller/HioReservInsertController.java
@@ -1,89 +1,48 @@
 package hio.controller;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
 import hio.HioMain;
 import hio.domain.HioMember;
-import hio.service.HioMemberInsertService;
 import hio.service.HioReservInsertService;
 
+//HIOtest 프로젝트
 public class HioReservInsertController implements HioReservInsertInterface{
 
-	public int reservInsert() {
-		
-		int result = -1;
+	HioReservInsertService service = new HioReservInsertService();
+
+	public int insertReserv() {
+		int result = 0;
 		
 		HioMember hioMember = HioMain.hioMember;
 		
-		Scanner sc = new Scanner(System.in);
-			
-			System.out.println("------------------------------------------------");
-			System.out.println("-------------------- 타석예약 --------------------");
-			
-			System.out.println(hioMember.getMemberGrade());
-			System.out.println(hioMember.getMemberNo());
-			
-			List<HioMember> hallList = new HioReservInsertService().selectHallList();
-			System.out.println("------------------- 예약가능 홀 -------------------");
-			
-			for(HioMember hio : hallList) {
-				System.out.print("| "+ hio.getHallNo() + "번 홀 |\t");
-			}
-			System.out.println();
-			
-			System.out.print("예약하실 홀 번호를 입력해주세요 >>");
-			int hallno = Integer.parseInt(sc.nextLine());
-			
-			HioMember selectHallTime = new HioReservInsertService().selectHallTime(hioMember, hallno);
-			
-			List<HioMember> resvTimeList =new HioReservInsertService().selectResvTimeList(hallno);
-			
-			int stime = hioMember.getHallsTime();
-			int etime = hioMember.getHalleTime();
-				System.out.println("------------------------------------------------");
-			for(int a=stime; a<=etime; a++) {
-				boolean resv = false;
-				for(HioMember resvtime : resvTimeList) {
-					if(resvtime.getReservTime() == a) {
-						System.out.println("|----- "+a+"시 --------------------- 예약불가 -------|");
-						resv = true;
-						break;
-					}
-				}
-				if(!resv) {
-					System.out.println("|----- "+a+"시 --------------------- 예약가능 -------|");
-				}
-				
-			}
-				System.out.println("------------------------------------------------");
-			
-			int halltime = 0;
-			boolean chk = false;
-			while(true) {
-				System.out.print("예약하실 시간을 입력해주세요 >>");
-				halltime = Integer.parseInt(sc.nextLine());
-				
-				for(HioMember resvTime : resvTimeList) {
-					if(resvTime.getReservTime() == halltime) {
-						chk = true;
-						System.out.println("이미 예약되어있는 시간입니다.");
-						break;
-					}
-					else {
-						chk = false;
-					}
-				}
-				if(!chk) {
-					break;
-				}
-			}
-			
-			hioMember.setHallNo(hallno);
-			hioMember.setReservTime(halltime);
-			
-			result = new HioReservInsertService().reservInsert(hioMember);
-
+		System.out.println("타석 예약을 진행합니다.");		
 		
-			return result;
+		List<HioMember> hallList = new ArrayList<>();
+		List<HioMember> reservList = new ArrayList<>();
+		List<HioMember> memList = new ArrayList<>();
+		
+		hallList = service.possReservListHall();
+		reservList = service.possReservListReserv();
+		memList = service.possReservListMem();
+		
+		System.out.println("---------- 예약 가능한 홀 ----------");
+		for(HioMember h : hallList) {
+			System.out.print("|" + h.getHallNo() + "번 홀 |\t");
 		}
+		
+		
+		
+		//if (reservListHall != null && !reservListHall.isEmpty()) {
+		//	for(HioMember h : reservListHall) {
+		//		System.out.println(h);
+		//	}	
+		//}
+		
+		
+		
+		
+		return result;
+	}
 }
