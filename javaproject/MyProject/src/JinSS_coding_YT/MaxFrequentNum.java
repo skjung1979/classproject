3bdc912f993f096328fe5b527772b46165c65a99
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-31 15:59:43 +0900

    Create MaxFrequentNum.java

---
+++ javaproject/MyProject/src/JinSS_coding_YT/MaxFrequentNum.java
@@ -0,0 +1,47 @@
+package JinSS_coding_YT;
+
+import java.util.Scanner;
+
+public class MaxFrequentNum {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("10개의 숫자를 입력해주세요. >> ");
+
+        int[] inputNum = new int[10];
+
+        // for (int i=0; i<10; i++){
+
+        //     inputNum[i] = Integer.parseInt(sc.nextLine());
+
+        // }
+
+        inputNum = {1, 1, 2, 3, 1, 4, 2, 4, 1, 5};
+
+        int[] mode = new int[10];
+
+        for (int i=0; i<10; i++){
+
+            mode[inputNum[i]] += 1; // inputNum의 값을 인덱스로하는 mode를 1씩 증가시킨다.
+
+        }
+
+        int maxNum = 0; // 최대 빈도 수
+        int maxCnt = 0; // 최대 빈도 횟수
+
+        for (int i=0; i<10; i++){
+
+            if (maxCnt < mode[i]){
+
+                maxCnt = mode[i];
+            maxNum = i;
+
+            }
+        }
+        System.out.println("최빈수: " + maxNum);
+        System.out.println("최빈수 횟수: " + maxCnt);
+
+    } 
+}
