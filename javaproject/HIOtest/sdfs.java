bd40704b6a6a04c05a40cdc9be2e55ebea7e2624
skjung1979 <skjung1979.dvlp@gmail.com>
2022-11-11 00:07:35 +0900

    초기세팅들

---
+++ javaproject/HIOtest/sdfs.java
@@ -0,0 +1,12 @@
+
+
+pom.xml
+
+
+
+<!-- https://mvnrepository.com/artifact/com.jcraft/jsch -->
+<dependency>
+<groupId>com.jcraft</groupId>
+<artifactId>jsch</artifactId>
+<version>0.1.55</version>
+</dependency>
