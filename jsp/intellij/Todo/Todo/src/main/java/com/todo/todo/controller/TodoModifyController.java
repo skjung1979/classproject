0588a47aa0ad9632b2387ad0eea432a2fa69146c
skjung1979 <skjung1979.dvlp@gmail.com>
2022-11-23 13:12:46 +0900

    formatter(날짜) 적용 완료

--- jsp/intellij/Todo/Todo/src/main/java/com/todo/todo/controller/TodoModifyController.java
+++ jsp/intellij/Todo/Todo/src/main/java/com/todo/todo/controller/TodoModifyController.java
@@ -4,9 +4,11 @@ import com.todo.todo.domain.TodoDTO;
 import com.todo.todo.service.TodoService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -27,29 +29,31 @@ public class TodoModifyController {
     @GetMapping
     public String putModify(
             HttpServletRequest request,
-            HttpServletResponse response
-    ) {
+            Model model,
+            @RequestParam("tno") int tno
+    ) throws Exception {
 
         System.out.println("modify get 들어옴");
 
         // 사용자 요청 tno 받기
         //String tno = request.getParameter("tno");
-
         // service 요청한 tno로 할일 목록을 검색해서 결과 -> TodoDTO로 반환
         //TodoDTO todo = new TodoDTO(1, "공부하기", "열심히 공부하기", "2022-11-26", false); // 받아왔다 치고 임시로 이 로직 삽입.
-
-        int tno = Integer.parseInt(request.getParameter("tno"));
+        //int tno = Integer.parseInt(request.getParameter("tno")); 
+        // 이상 주석 => @RequestParam으로 대체함
+        
         log.info("요청 번호: " + tno);
 
-        TodoDTO result = new TodoDTO();
-
+    /*    TodoDTO result = new TodoDTO();
         try {
             result = service.selectBy(tno);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        request.setAttribute("todo", result);*/
+        // 이상 주석 => model.addAttribute("todo", service.selectBy(tno)); 로 대체함
 
-        request.setAttribute("todo", result);
+        model.addAttribute("todo", service.selectBy(tno));
 
         return "todo/modify";
 
@@ -58,40 +62,36 @@ public class TodoModifyController {
 
     @PostMapping
     public String postModify(
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) {
+//            HttpServletRequest request,
+//            HttpServletResponse response
+            TodoDTO todoDTO
+    ) throws Exception {
 
         log.info("modify post 들어옴");
 
         // 사용자가 수정한 데이터를 업데이트 처리
-        String tno = request.getParameter("tno");
-        String todo = request.getParameter("todo");
-        String memo = request.getParameter("memo");
-        String dueDate = request.getParameter("dueDate");
-        String finished = request.getParameter("finished");
+//        String tno = request.getParameter("tno");
+//        String todo = request.getParameter("todo");
+//        String memo = request.getParameter("memo");
+//        String dueDate = request.getParameter("dueDate");
+//        String finished = request.getParameter("finished");
+        // 이상 주석 => TodoDTO로 대체함
 
-        log.info("수정 요청 받은 번호: " + tno);
-        log.info("수정 요청 받은 할일: " + todo);
-        log.info("수정 요청 받은 설명: " + memo);
-        log.info("수정 요청 받은 기한: " + dueDate);
-        log.info("수정 요청 받은 완료여부: " + finished);
+        log.info("수정 요청 받은 번호: " + todoDTO.getTno());
+        log.info("수정 요청 받은 할일: " + todoDTO.getTodo());
+        log.info("수정 요청 받은 설명: " + todoDTO.getMemo());
+        log.info("수정 요청 받은 기한: " + todoDTO.getDueDate());
+        log.info("수정 요청 받은 완료여부: " + todoDTO.isFinished());
 
         // Service로 보낼 DTO 생성 <- 사용자가 입력한 수정값으로 DTO객체를 생성해서 서비스로 보내는 것이다.
-        TodoDTO dto = new TodoDTO(Long.parseLong(tno), todo, memo, dueDate, finished == null ? false : true);
+        //TodoDTO dto = new TodoDTO(Long.parseLong(tno), todo, memo, dueDate, finished == null ? false : true);
 
-        int result = 0;
+        log.info(todoDTO);
 
-        System.out.println(dto);
+        service.updateTodo(todoDTO);
 
-        try {
-            result = service.updateTodo(dto);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        if (result > 0) {
-            System.out.println("도서 변경 완료!!!!");
+        if (service.updateTodo(todoDTO) > 0) {
+            log.info("변경 사항 DB적용 완료!!!!");
         }
 
         // 수정되면 리스트로 보낸다.
--- jsp/intellij/Todo/Todo/src/main/java/com/todo/todo/formatter/LocalDateFormatter.java
+++ jsp/intellij/Todo/Todo/src/main/java/com/todo/todo/formatter/LocalDateFormatter.java
@@ -1,18 +1,17 @@
-package com.firstcoding.mvc.springmvc.formatter;
+package com.todo.todo.formatter;
 
 import org.springframework.format.Formatter;
+
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class LocalDateFormatter implements Formatter<LocalDate> {
-
     @Override
     public LocalDate parse(String text, Locale locale) throws ParseException {
-        // 문자타입 날짜를 받아서 LocalDate 타입으로 변환하여 반환한다!
-
-        return LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd")); // 전달받은 text를 원하는 날짜 패턴으로 parse한다.
+        // 문자 타입의 날짜를 받아서 LocalDate 타입으로 변환하여 반환한다.
+        return LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
     @Override
