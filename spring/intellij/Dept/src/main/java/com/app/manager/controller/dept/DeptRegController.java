4367de243bf9c6e34fa22acb6ca6c625acb3e033
skjung1979 <skjung1979.dvlp@gmail.com>
2022-11-25 12:32:00 +0900

    validation 설정

--- spring/intellij/Dept/src/main/java/com/app/manager/controller/dept/DeptRegController.java
+++ spring/intellij/Dept/src/main/java/com/app/manager/controller/dept/DeptRegController.java
@@ -5,9 +5,13 @@ import com.app.manager.service.dept.DeptRegService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Log4j2
 @Controller
@@ -24,14 +28,25 @@ public class DeptRegController {
 
     @PostMapping
     public String reg(
-            DeptDTO deptDTO
+            @Valid DeptDTO deptDTO,
+            BindingResult bindingResult
     ){
 
         log.info("reg Post 진입");
         log.info(deptDTO);
 
-        //deptRegService.insertDept(deptDTO);
-        deptRegService.insertDepts(deptDTO);
+        if (bindingResult.hasErrors()){ // 정상이라면 이 값은 false로 들어옴 => true이면 에러가 발생한 것!!!
+            log.info(bindingResult.getAllErrors());
+            for (ObjectError objectError : bindingResult.getAllErrors()){
+
+                log.info(objectError.getCodes()[1]+ " ==> " + objectError.getDefaultMessage()); // => 에러 메세지 표시
+                // 문제가 발생하는 상황이므로 리다이렉션 시켜서 다시 입력을 받든지 내부에서 전처리 하든지 결정한다.
+
+            }
+        };
+
+        deptRegService.insertDept(deptDTO);
+        //deptRegService.insertDepts(deptDTO);
 
         return "redirect:/dept/list";
     }
--- spring/intellij/Dept/src/main/java/com/app/manager/domain/DeptDTO.java
+++ spring/intellij/Dept/src/main/java/com/app/manager/domain/DeptDTO.java
@@ -2,6 +2,8 @@ package com.app.manager.domain;
 
 import lombok.*;
 
+import javax.validation.constraints.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -10,8 +12,19 @@ import lombok.*;
 @Builder
 public class DeptDTO {
 
+    @Min(value = 1) // 숫자의 경우 최소값, 최대값 정의 가능!
+    @Max(value = 99)
+    @Positive // 양수값만 들어오도록 or @Negative // 음수값만 들어오도록
     private int deptno;
-    private String dname;
-    private String loc;
 
+    @NotEmpty // 문자열이 비어있지 않은지 상태 확인, List의 경우 요소의 개수가 비어 있는지 체킹
+    private String dname;
+
+    @NotEmpty
+    private String loc;
 }
+// validation annotation
+// @Future : 현재 시간보다 미래이어야 한다. ex) ToDo리스트 입력 시 날짜 항목에서 현재 보다 미래이어야 하는 경우
+// @Email : 이메일 체크, @부분 외에 전체 형식 체킹
+// @NotBlank : null이 아닌 상태에서, 공백 문자열 체크 " " 이것은 허용 안됨!
+// @NotNull: null값이 아니어야 한다
