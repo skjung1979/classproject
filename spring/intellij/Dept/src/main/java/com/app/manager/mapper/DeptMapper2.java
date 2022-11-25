1cb1cf6e3e253715d6a02fe8eb862305689c3e11
skjung1979 <skjung1979.dvlp@gmail.com>
2022-11-25 10:34:33 +0900

    mapper.xml없이 쿼리문 실행 -> 어노테이션 사용

---
+++ spring/intellij/Dept/src/main/java/com/app/manager/mapper/DeptMapper2.java
@@ -0,0 +1,34 @@
+package com.app.manager.mapper;
+
+import com.app.manager.domain.DeptDTO;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface DeptMapper2 {
+
+    // 간단한 것은 mapper.xml만들 필요없이 어노테이션을 사용해도 된다.
+    // 단, 잘 바뀌지 않는 간단한 코드들만 왜냐하면 빌드 및 컴파일을 다시해야 하므로
+    @Select("select * from dept")
+
+    // 전체 부서 리스트
+    List<DeptDTO> selectAll();
+
+    // 수정 or 상세페이지
+    @Select("select * from dept where deptno=#{no}")
+    DeptDTO selectByDeptno(int deptno);
+
+    // 수정(변경) 처리
+    @Update("update dept set dname=#{dname}, loc=#{loc} where deptno=#{deptno}")
+    int updateDept(DeptDTO deptDTO);
+
+    // 삭제
+    @Delete("delete from dept where deptno=#{no}")
+    int deleteByDeptno(int deptno);
+
+    // 입력
+    @Insert("insert into dept values(#{deptno}, #{dname}, #{loc})")
+    int insertDept(DeptDTO deptDTO);
+
+}
--- spring/intellij/Dept/src/main/java/com/app/manager/service/dept/DeptListService.java
+++ spring/intellij/Dept/src/main/java/com/app/manager/service/dept/DeptListService.java
@@ -2,6 +2,7 @@ package com.app.manager.service.dept;
 
 import com.app.manager.domain.DeptDTO;
 import com.app.manager.mapper.DeptMapper;
+import com.app.manager.mapper.DeptMapper2;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,12 @@ public class DeptListService {
     @Autowired(required = false)
     private DeptMapper deptMapper;
 
+    @Autowired(required = false)
+    private DeptMapper2 deptMapper2;
+
     public List<DeptDTO> getList(){
 
-        return deptMapper.selectAll();
+        return deptMapper2.selectAll();
     }
 
 
