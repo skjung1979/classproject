a68189289101e574133d0b5ee0fde0c84ce81632
skjung1979 <skjung1979.dvlp@gmail.com>
2022-11-30 12:59:06 +0900

    SpringRestPRJ / 포매터 파일 추가

---
+++ spring/intellij/SpringRestPRJ/src/main/java/com/bbs/springrestprj/formatter/LocalDateFormatter.java
@@ -0,0 +1,21 @@
+package com.bbs.springrestprj.formatter;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class LocalDateFormatter implements Formatter<LocalDate> {
+    @Override
+    public LocalDate parse(String text, Locale locale) throws ParseException {
+        // 문자 타입의 날짜를 받아서 LocalDate 타입으로 변환하여 반환한다.
+        return LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    @Override
+    public String print(LocalDate object, Locale locale) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd").format(object);
+    }
+}
