85de052896b770bda875724ea849021bcc561fe2
skjung1979 <skjung1979.dvlp@gmail.com>
2022-12-14 17:55:44 +0900

    JPA 수업

--- spring/boot/FirstBootApp/src/test/java/com/first/app/DeptRepositoryTest.java
+++ spring/boot/FirstBootApp/src/test/java/com/first/app/DeptRepositoryTest.java
@@ -6,8 +6,11 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import java.awt.print.Pageable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -117,6 +120,26 @@ public class DeptRepositoryTest {
         List list3 = deptRepository.findByDeptnoBetween(30,60);
 
         log.info(">>>>>>>>>>>>>>> " + list3);
+
+        log.info(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+
+
+
+    }
+
+    @Test
+    public void testPaging(){
+
+        Pageable pageable = PageRequest.of(2, 4, Sort.by("deptno").descending());
+
+        Page<Dept> result = deptRepository.findAll(pageable);
+
+        log.info("result >>> 조회 항목 리스트 getContent " + result.getContent());
+        log.info("result >>> 전체 페이지 개수 getTotalPages " + result.getTotalPages());
+        log.info("result >>> 전체 게시물 개수 getTotalElements " + result.getTotalElements());
+        log.info("result >>> 요청 페이지 getNumber " + result.getNumber());
+        log.info("result >>> 해당 페이지의 항목 개수 getNumberOfElements " + result.getNumberOfElements());
+        log.info("result >>> 페이지당 표현 항목 개수 getSize " + result.getSize());
 
         log.info(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
 
