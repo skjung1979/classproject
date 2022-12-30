37c24b731749259b36a963f9dd41043634efdeb8
skjung1979 <skjung1979.dvlp@gmail.com>
2022-12-30 17:41:55 +0900

    React Todo 수업

---
+++ react/todo/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Ex1 from './components/ex/Ex1';
+import SamplePage from "./page/SamplePage";
+
+function App() {
+  return (
+    <SamplePage/>
+  );
+}
+
+export default App;
