ae07fcfd0a7b9a7669eb62b4051eb6083c0c535b
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-02 09:32:36 +0900

    React seomal 연습

--- react/seomal/src/App.js
+++ react/seomal/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
+import styled from 'styled-components';
 
 function Header({title, body, onChangeMode}) {
     return (
@@ -16,20 +17,17 @@ function Header({title, body, onChangeMode}) {
 
 function Nav({navDatas, onChangeMode}) {
 
-    const list = [
-        navDatas.map(item => {
-            return <li key={item.id}><a id={item.id} href={'/read/' + item.id} onClick={(e) => {
-                e.preventDefault()
-                onChangeMode(e.target.id)
-            }
-            }>{item.title}</a></li>
-        })
-    ]
-
-    // map으로 하거나 반복문을 돌린다.
-    // for(let i=0; i<navDatas.length; i++){
-    //     list.push(<li><a href={'/read/'+navDatas[i].id}>{navDatas[i].title}</a></li>)
-    // }
+    const list = []
+    for (let i = 0; i < navDatas.length; i++) {
+        let t = navDatas[i];
+        list.push(<li key={t.id}>
+                <a id={t.id} href={'/read/' + t.id} onClick={e => {
+                    e.preventDefault();
+                    onChangeMode(Number(e.target.id));
+                }}>{t.title}</a>
+            </li>
+        );
+    }
 
     return (
         <nav>
@@ -49,25 +47,67 @@ function Article({title, body}) {
     )
 }
 
+function Create(props) {
+    return (
+        <article>
+            <h2>Create</h2>
+            <form onSubmit={e => {
+                e.preventDefault();
+                const title = e.target.title.value;
+                const body = e.target.body.value;
+                props.onCreate(title, body);
+            }}>
+                <p><input type="text" name="title" placeholder="제목을 입력하세요."/></p>
+                <p><textarea name="body" placeholder="본문을 입력하세요." rows="5" cols="22"/></p>
+                <p><input type="submit" value="Create"/></p>
+            </form>
+        </article>
+    )
+}
+
 const headerData = {title: 'WEB', body: 'this is Header.'}
-const navDatas = [
-    {id: 1, title: 'html5', body: 'html5 is...'},
-    {id: 2, title: 'css', body: 'css is...'},
-    {id: 3, title: 'javascript', body: 'javascript is...'}
-]
 const articleData = {title: 'Welcome', body: '안녕하세요. 메인페이지 입니다.'}
 const articleData2 = {title: 'Read', body: '안녕하세요. Read!!!!'}
 
 function App() {
 
     const [mode, setMode] = useState('WELCOME')
+    const [idx, setIdx] = useState(null) /*현재 값이 선택된게 없으니깐 null*/
+    const [nextIdx, setNextIdx] = useState(4)
+    const [navDatas, setNavDatas] = useState([
+            {id: 1, title: 'html5', body: 'html5 is...'},
+            {id: 2, title: 'css', body: 'css is...'},
+            {id: 3, title: 'javascript', body: 'javascript is...'}
+        ]
+    )
 
     let content = null;
 
     if (mode === 'WELCOME') {
         content = <Article title={articleData.title} body={articleData.body}/>
     } else if (mode === 'READ') {
-        content = <Article title={articleData2.title} body={articleData2.body}/>
+        let title, body = null
+        for (let i = 0; i < navDatas.length; i++) {
+            if (navDatas[i].id === idx) {
+                title = navDatas[i].title
+                body = navDatas[i].body
+                console.log('id / idx => ', navDatas[i].id, idx)
+            }
+        }
+        console.log('title => ', title)
+        console.log('body => ', body)
+
+        content = <Article title={title} body={body}/>
+    } else if (mode === 'CREATE') {
+        content = <Create onCreate={(_title, _body) => {
+            const newNavData = {id: nextIdx, title: _title, body: _body}
+            const newNavDatas = [...navDatas] /*오리지널 데이터를 복제한다.*/
+            newNavDatas.push(newNavData) /*오리지널 데이터에 새로운 입력받은 데이터를 넣는다.*/
+            setNavDatas(newNavDatas)
+            setMode('READ')
+            setIdx(nextIdx)
+            setNextIdx(nextIdx + 1)
+        }}></Create>
     }
 
     return (
@@ -80,9 +120,17 @@ function App() {
             <Nav navDatas={navDatas} onChangeMode={(id) => {
                 // alert(id)
                 // mode = 'READ';
-                setMode('READ')
+                setMode('READ');
+                setIdx(id);
+                console.log('idx', idx)
             }}/>
             {content}
+            <br/>
+            <a href="/create" onClick={e => {
+                e.preventDefault();
+                setMode('CREATE');
+                console.log('mode => ', mode)
+            }}>create</a>
         </div>
     );
 }
---
+++ react/seomal2/src/App.js
@@ -0,0 +1,157 @@
+import './App.css';
+import {useState} from "react";
+import styled from "styled-components";
+
+// const Create = styled.button`
+//     margin: 10px;
+// `
+
+function Header({title, body, onChangeMode}) {
+    return (
+        <header>
+            <h1 style={{display: 'inline'}}>
+                <a href="/" onClick={e => {
+                    e.preventDefault();
+                    onChangeMode()
+                }}>{title}</a>
+            </h1>
+            {body}
+        </header>
+    )
+}
+
+function Nav({navDatas, onChangeMode}) {
+    const list = [];
+
+    navDatas.map(item => {
+        list.push(
+            <li key={item.id}>
+                <a id={item.id}
+                   href={'/read/' + item.id}
+                   onClick={e => {
+                       e.preventDefault();
+                       onChangeMode(Number(e.target.id));
+                   }}>
+                    {item.title}
+                </a>
+            </li>
+        )
+    })
+
+    /*for (let i = 0; i < navDatas.length; i++) {
+        let n = navDatas[i];
+        list.push(
+            <li key={n.id}>
+                <a id={n.id}
+                   href={'/read/' + n.id} onClick={e => {
+                    e.preventDefault();
+                    onChangeMode(Number(e.target.id)); // a태그를 클릭하면 id속성의 값을 onChanedMode함수에 매개변수로 담아 호출한다!
+                }}>
+                    {n.title}
+                </a>
+            </li>
+        )
+    }*/
+
+
+    return (
+        <nav>
+            <ol>
+                {list}
+            </ol>
+        </nav>
+    )
+}
+
+function Article({title, body}) {
+    return (
+        <article>
+            <h2>{title}</h2>
+            {body}
+        </article>
+    )
+}
+
+function Create({onCreate}) {
+    return (
+        <article>
+            <h2>Create</h2>
+            <form onSubmit={e => {
+                e.preventDefault();
+                const title = e.target.tt.value;
+                const body = e.target.ta.value;
+                onCreate(title, body)
+            }}>
+                <p><input type="text" name="tt" placeholder="제목을 입력하세요."/></p>
+                <p><textarea name="ta" placeholder="본문을 입력하세요."></textarea></p>
+                <input type="submit" value="글작성"/>
+            </form>
+        </article>
+    )
+}
+
+const headerData = {title: 'WEBSITE', body: ' this is header'}
+const welcomeData = {title: 'Welcome', body: '안녕하세요. 메인 페이지 입니다.'}
+
+function App() {
+
+    const [mode, setMode] = useState('HOME');
+    const [navDatas, setNavDatas] = useState([
+        {id: 1, title: 'html5', body: 'html5 is ............'},
+        {id: 2, title: 'css', body: 'css is ............'},
+        {id: 3, title: 'javascript', body: 'javascript is ............'}
+    ])
+    const [idx, setIdx] = useState(null);
+    const [nextIdx, setNextIdx] = useState(4)
+
+    let content = null;
+
+    if (mode === 'HOME') {
+        content = <Article title={welcomeData.title} body={welcomeData.body}/>
+    } else if (mode === 'READ') {
+        // 분기
+        let title, body = null;
+
+        navDatas.map(item => {
+            if (item.id === idx) {
+                title = item.title;
+                body = item.body;
+            }
+        })
+
+        content = <Article title={title} body={body}/>
+    } else if (mode === 'CREATE'){
+        content = <Create onCreate={(tt, ta) => {
+            const newNavData = {id: nextIdx, title: tt, body: ta}
+            const newNavDatas = [...navDatas]
+            newNavDatas.push(newNavData)
+            setNavDatas(newNavDatas)
+            setMode('READ')
+            setIdx(nextIdx)
+            setNextIdx(nextIdx + 1)
+        }
+        }></Create>
+    }
+
+    return (
+        <div>
+            <Header title={headerData.title} body={headerData.body} onChangeMode={() => {
+                setMode('HOME')
+            }}></Header>
+            <Nav navDatas={navDatas} onChangeMode={(id) => {
+                setMode('READ');
+                setIdx(id);
+            }}/>
+            {content}
+            <br/>
+            <a
+                href="/create" onClick={e => {
+                    e.preventDefault();
+                    setMode('CREATE');
+            }}
+            >create</a>
+        </div>
+    ); // app returen close
+}
+
+export default App;
